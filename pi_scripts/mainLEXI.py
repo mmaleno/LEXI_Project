bc9534850cbad3a69919c82285a890b468f52b5a
Max Maleno <mmaleno@hmc.edu>
2018-07-22 21:28:30 -0700

    Added a few new ideas to TODO

--- pi_scripts/mainLEXI.py
+++ pi_scripts/mainLEXI.py
@@ -4,10 +4,15 @@
 # TODO:  -remove copious print statements (maybe not, they've been helpful)
 #        -display text of "real word" location (ex: "driveway", "backyard", etc.),
 #        -put tracker connection pending text on next line (have text wrap)
+#        -instead of changing mac/pi every time, make a system-specific conditional
+#        -make system compare current time to last successful transmission time,
+#         if it is more than 10-15 seconds, then have tracker disconnected
 #        -Instead of 00:00:00 NEVER, make just "NEVER"
+#        -prevent pi from turning off screen after idle time (IMPORTANT)
 #        -display wifi signal strength of tracker
 #        -display battery voltage
 #        -ENABLE REMOTE TALKING TO LEXI?????? (need mic and speaker)
+#        -Add multiple-dog functionality (lowest priority)
 #       Allow multiple files by passing fig object as argument in function (like FDIR)
 #       Add auto-startup - need to add switch/button to RUN pins onboard the pi
 
